fa334e0bfe94e82d799889c1c6723f8880e8f28b
Travis Hull <travish@ku.edu>
2019-03-01 10:17:45 -0600

    implementing simple ex1

--- lab3_ex1.js
+++ lab3_ex1.js
@@ -1,12 +1,21 @@
-/*
-  var person = prompt("Please enter your name", "Harry Potter");
+function buttonFunc()
+{
+  let password1 = document.getElementById("password1").value;
+  let password2 = document.getElementById("password2").value;
 
-  if (person == null || person == "")
-  {
-    txt = "User cancelled the prompt.";
-  }
-  else {
-    txt = "Hello " + person + "! How are you today?";
-  }
-alert(txt);
-*/
+//  if (password1.length() < 8)
+//  {
+//    alert("Please create a password which is at least 8 characters long!");
+//  }
+//  else
+//  {
+    if (password1 == password2)
+    {
+      alert("Password OK!");
+    }
+    else
+    {
+      alert("Please ensure both passwords are the same!");
+    }
+//  }
+}
