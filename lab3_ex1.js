999c322c538a28f9dd0a60d63b4478e86d349802
Travis Hull <travish@ku.edu>
2019-03-01 10:19:20 -0600

    added password length check

--- lab3_ex1.js
+++ lab3_ex1.js
@@ -3,12 +3,12 @@ function buttonFunc()
   let password1 = document.getElementById("password1").value;
   let password2 = document.getElementById("password2").value;
 
-//  if (password1.length() < 8)
-//  {
-//    alert("Please create a password which is at least 8 characters long!");
-//  }
-//  else
-//  {
+  if (password1.length() < 8)
+  {
+    alert("Please create a password which is at least 8 characters long!");
+  }
+  else
+  {
     if (password1 == password2)
     {
       alert("Password OK!");
@@ -17,5 +17,5 @@ function buttonFunc()
     {
       alert("Please ensure both passwords are the same!");
     }
-//  }
+  }
 }
