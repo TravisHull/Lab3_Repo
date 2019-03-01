39c4f78656036db33d90e141c363edc785f9ec8c
Travis Hull <travish@ku.edu>
2019-03-01 10:21:34 -0600

    fixed small syntax error in buttonFunc

--- lab3_ex1.js
+++ lab3_ex1.js
@@ -3,7 +3,7 @@ function buttonFunc()
   let password1 = document.getElementById("password1").value;
   let password2 = document.getElementById("password2").value;
 
-  if (password1.length() < 8)
+  if (password1.length < 8)
   {
     alert("Please create a password which is at least 8 characters long!");
   }
