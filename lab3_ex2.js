6fc2b70c8236663d84bd276d36e8ab28fd56e93c
Travis Hull <travish@ku.edu>
2019-03-01 13:36:16 -0600

    slideshow wip

--- lab3_ex2.js
+++ lab3_ex2.js
@@ -16,6 +16,7 @@ function showSlides(n)
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
+  
   if (n > slides.length)
   {
     slideIndex = 1;
